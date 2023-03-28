06caf02a7605c279eeea24a9276680977fef80d5
otee042 <onthatile.totwe2@gmail.com>
2023-03-28 09:33:07 +0200

    creating folder

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
-const number = document.querySelector('.counter_value')
-const subtract =
-const add =
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
+
+const subtractHandler = () => {
+    number.value = number.value - 1;
+}
+
+const addHandler = () => {
+    number.value = number.value + 1;
+}
+
+subtract.addEventListener('click', subtractHandler)
+add.addEventListener('click', addHandler)
