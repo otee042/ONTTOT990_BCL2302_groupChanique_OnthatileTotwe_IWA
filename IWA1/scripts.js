4d94d579d91fe47201b1ed44522dfe24079e730c
otee042 <onthatile.totwe2@gmail.com>
2023-03-28 11:23:36 +0200

    adding step amount

--- IWA1/scripts.js
+++ IWA1/scripts.js
@@ -1,13 +1,39 @@
+const MAX_NUMBER = 10
+const MIN_NUMBER = -10
+const STEP_AMOUNT = 1
+
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
 const subtractHandler = () => {
-    number.value = number.value - 1;
+    const newvalue = parseInt(number.value) - STEP_AMOUNT
+    number.value = newvalue 
+
+    if (add.disabled === true) {
+        add.disabled = false
+    }
+
+    if (newvalue <= MIN_NUMBER) {
+        subtract.disabled = true
+    } else {
+        subtract.disabled = false
+    }
 }
 
 const addHandler = () => {
-    number.value = number.value + 1;
+    const newvalue = parseInt(number.value)  + STEP_AMOUNT
+    number.value = newvalue
+
+    if (subtract.disabled === true) {
+        subtract.disabled = false
+    }
+
+    if (newvalue >= MAX_NUMBER) {
+        add.disabled = true
+    }
+        
+
 }
 
 subtract.addEventListener('click', subtractHandler)
