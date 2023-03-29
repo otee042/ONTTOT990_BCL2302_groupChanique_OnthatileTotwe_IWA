11c9d28a123aa50cf00a56a5325f03333654e540
otee042 <onthatile.totwe2@gmail.com>
2023-03-29 11:01:50 +0200

    correcting a comment

--- IWA2/Challenge-3/script.js
+++ IWA2/Challenge-3/script.js
@@ -7,6 +7,6 @@ It is important to show the following message in order to:
 
 console.warn("Security scan starting")
 
-/* It is important to let user know when they can close the page  */
+// It is important to let user know when they can close the page  
 
 console.info('Please wait for scan to complete before closing the browser')
