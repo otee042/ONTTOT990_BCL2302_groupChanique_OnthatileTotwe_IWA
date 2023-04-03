9f94309efa03729c62043ca9076b1d405da737f0
otee042 <onthatile.totwe2@gmail.com>
2023-04-03 16:15:05 +0200

    fixing errors

--- IWA5/script.js
+++ IWA5/script.js
@@ -1,17 +1,20 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = '0'
+const customers = '1'
+let location = 'RSA'
+currency = null
 
 if (location = RSA) { shipping === 400 && currency === 'R' }
 
-if location = NAM
+if (location = NAM)
 shipping = 600 
 else shipping = 800
 
 shoes = 300 * 1
 toys - 100 * 5
 shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
+batteries = 35 * 2
 pens = 5 * 'NONE_SELECTED' 
 
 shipping = null
@@ -23,12 +26,13 @@ if (shoes + batteries + pens + shirts > 1000 &&  ) {
 		    shipping = 0 || calcShipping
 		}
 	}
+
+
+if (shipping = 0) && (customers !== 1) { 
+    console.log(WARNING) }
+
+if (location = 'NK') {
+    console.log(WARNING)  
+    console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
-
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-
-customers = '1'
-const location = 'RSA'
-currency = null
