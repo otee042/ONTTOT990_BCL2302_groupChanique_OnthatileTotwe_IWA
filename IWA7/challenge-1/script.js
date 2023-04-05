bfcf9cc18705708480ab847f24b782c7a5aa3761
otee042 <onthatile.totwe2@gmail.com>
2023-04-05 16:23:31 +0200

    completed challenge2

---
+++ IWA7/challenge-1/script.js
@@ -0,0 +1,3 @@
+const value = 3     // changed value to a number
+
+console.log(` ${value + 4 + value}` )       //used interpolation 
---
+++ IWA7/challenge-2/script.js
@@ -0,0 +1,11 @@
+const nickname = "Timmy";
+const firstname = "Timothy";
+const message = "Good Morning";
+
+if (nickname === true){
+    console.log(message , nickname )
+} else if (nickname !== true ) {
+            console.log(message, firstname )
+}else console.log(message)
+
+//console.log(`Good Morning , ${nickname}  ${firstname} `)
