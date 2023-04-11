bffff311e0e39066dfa85a8cccc28ee3fe166dd1
otee042 <onthatile.totwe2@gmail.com>
2023-04-11 16:08:49 +0200

    creating folder of iwa9

--- IWA8/script.js
+++ IWA8/script.js
@@ -15,7 +15,7 @@ const leo = {
      name: leoName,
      balance: leoBalance,
      accessId: '47afb389-8014-4d0b-aff3-e40203d2107f',
-     age: '24',
+     age: 24,
     address: {
          number: leoNumber,
          street: leoStreet,
