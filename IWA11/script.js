723f308a04ff54910a11d585392a47433261b289
otee042 <onthatile.totwe2@gmail.com>
2023-04-14 16:13:50 +0200

    completing iwa11

--- IWA11/script.js
+++ IWA11/script.js
@@ -1,4 +1,4 @@
-const order1 = document.querySelector('[.date-key = "order1"]');        //ORDER 1
+/*const order1 = document.querySelector('[.date-key = "order1"]');        //ORDER 1
 const biscuits1 = document.querySelector('[.data-biscuits="10"]');
 const donuts1 = document.querySelector('[.data-donuts = "13"]');
 const pancakes1 = document.querySelector('[.pancakes = "0"]');
@@ -36,6 +36,44 @@ pancakes1.innerText = pancakes1.getAttribute('[.data-pancakes="2"]')
 3-donuts = 3-root.donuts,
 3-pancakes = 3-root.pancakes,
 3-status = 3-root.status ? Delivered : Pending*/
+
+
+//for testing
+// script.js
+
+const order1 = document.querySelector('[data-key="order1"]');
+const biscuits1 = order1.querySelector('.biscuits .count');
+const donuts1 = order1.querySelector('.donuts .count');
+const pancakes1 = order1.querySelector('.pancakes .count');
+const status1 = order1.querySelector('.status dd');
+
+biscuits1.innerHTML = order1.getAttribute('data-biscuits');
+donuts1.innerHTML = order1.getAttribute('data-donuts');
+pancakes1.innerHTML = order1.getAttribute('data-pancakes');
+status1.innerHTML = order1.getAttribute('data-delivered');
+
+const order2 = document.querySelector('[data-key="order2"]');
+const biscuits2 = order2.querySelector('.biscuits .count');
+const donuts2 = order2.querySelector('.donuts .count');
+const pancakes2 = order2.querySelector('.pancakes .count');
+const status2 = order2.querySelector('.status dd');
+
+biscuits2.innerHTML = order2.getAttribute('data-biscuits');
+donuts2.innerHTML = order2.getAttribute('data-donuts');
+pancakes2.innerHTML = order2.getAttribute('data-pancakes');
+status2.innerHTML = order2.getAttribute('data-delivered');
+
+const order3 = document.querySelector('[data-key="order3"]');
+const biscuits3 = order3.querySelector('.biscuits .count');
+const donuts3 = order3.querySelector('.donuts .count');
+const pancakes3 = order3.querySelector('.pancakes .count');
+const status3 = order3.querySelector('.status dd');
+
+biscuits3.innerHTML = order3.getAttribute('data-biscuits');
+donuts3.innerHTML = order3.getAttribute('data-donuts');
+pancakes3.innerHTML = order3.getAttribute('data-pancakes');
+status3.innerHTML = order3.getAttribute('data-delivered');
+
  
 
  
--- IWA12/script.js
+++ IWA12/script.js
@@ -1,61 +1,126 @@
+// const STATUS_MAP = {
+//     shelf: {
+//         color: 'green',
+//         canReserve: true,
+//         canCheckout: true,
+//         canCheckIn: false,
+//     },
+//     reserved: {
+//         color: 'blue',
+//         canReserve: false,
+//         canCheckout: true,
+//         canCheckIn: false,
+//     },
+//     overdue: {
+//         color: 'red',
+//         canReserve: false,
+//         canCheckout: false,
+//         canCheckIn: true,
+//     },
+//     checkedOut: {
+//         color: 'orange',
+//         canReserve: false,
+//         canCheckout: false,
+//         canCheckIn: true,
+//     }
+// }
+
+// // Edit below line 
+
+// status = selector(status)
+// reserve = selector(reserve)
+// checkout = selector(checkout)
+// checkin = selector(checkin)
+
+// status = selector(status)
+// reserve = selector(reserve)
+// checkout = selector(checkout)
+// checkin = selector(checkin)
+
+// status = selector(status)
+// reserve = selector(reserve)
+// checkout = selector(checkout)
+// checkin = selector(checkin)
+
+// checkin.0.color = none
+// status.0.style.color = STATUS_MAP.status.color
+// reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+// checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+// checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+
+// checkin.1.color = none
+// status.1.style.color = STATUS_MAP.status.color
+// reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+// checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+// checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+
+// checkin.2.color = none
+// status.2.style.color = STATUS_MAP.status.color
+// reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
+// checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
+// checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+
+
+ 
+
+// check
+// scripts.js
+
 const STATUS_MAP = {
     shelf: {
-        color: 'green',
-        canReserve: true,
-        canCheckout: true,
-        canCheckIn: false,
+      color: 'green',
+      canReserve: true,
+      canCheckout: true,
+      canCheckIn: false,
     },
     reserved: {
-        color: 'blue',
-        canReserve: false,
-        canCheckout: true,
-        canCheckIn: false,
+      color: 'blue',
+      canReserve: false,
+      canCheckout: true,
+      canCheckIn: false,
     },
     overdue: {
-        color: 'red',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
+      color: 'red',
+      canReserve: false,
+      canCheckout: false,
+      canCheckIn: true,
     },
     checkedOut: {
-        color: 'orange',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
+      color: 'orange',
+      canReserve: false,
+      canCheckout: false,
+      canCheckIn: true,
     }
-}
-
-// Edit below line 
-
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
-checkin.0.color = none
-status.0.style.color = STATUS_MAP.status.color
-reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
-
-checkin.1.color = none
-status.1.style.color = STATUS_MAP.status.color
-reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
-
-checkin.2.color = none
-status.2.style.color = STATUS_MAP.status.color
-reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+  };
+  
+  // Get all the book containers
+  const bookContainers = document.querySelectorAll('div[id^="book"]');
+  
+  // Loop through the book containers
+  bookContainers.forEach(container => {
+    // Get the relevant elements in the container
+    const status = container.querySelector('.status');
+    const reserve = container.querySelector('.reserve');
+    const checkout = container.querySelector('.checkout');
+    const checkin = container.querySelector('.checkin');
+    
+    // Get the status of the book
+    const bookStatus = status.textContent.trim();
+    
+    // Get the corresponding status object from the STATUS_MAP
+    const statusObj = STATUS_MAP[bookStatus];
+    
+    // Set the color of the status text
+    status.style.color = statusObj.color;
+    
+    // Enable/disable buttons based on the status
+    reserve.disabled = !statusObj.canReserve;
+    checkout.disabled = !statusObj.canCheckout;
+    checkin.disabled = !statusObj.canCheckIn;
+    
+    // Set the color of the buttons to black and grayscale
+    reserve.style.color = reserve.disabled ? 'gray' : 'black';
+    checkout.style.color = checkout.disabled ? 'gray' : 'black';
+    checkin.style.color = checkin.disabled ? 'gray' : 'black';
+  });
+  
