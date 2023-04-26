b8d45000628c55a1bf2cdd72321261edf923eeff
otee042 <onthatile.totwe2@gmail.com>
2023-04-26 15:22:00 +0200

    iwa17

--- IWA16/script.js
+++ IWA16/script.js
@@ -65,50 +65,47 @@ const MONTHS = [
   
   // Only edit below this comment
   
-const Onthatile = () => {
-     data.NM372.firstName
+const createHtml = (athlete) => {
+  //  reassign the variables
+  const firstName = data.response.data[athlete].firstName;
+  const surname = data.response.data[athlete].surname;
+  const id = data.response.data[athlete].id;
+  const races = (data.response.data[athlete].races).length
+  const date = new Date(data.response.data[athlete].races[races-1].date)
+  const time = data.response.data[athlete].races[races-1].time;
+  const fragment = document.createDocumentFragment();              //Creating an empty DocumentFragment object.
+  let title = document.createElement('h2');        //Creates an h2 HTML element              
+  title.textContent = id                            //sets its text element to the id of the athlete
+  fragment.appendChild(title);                      //append it to the document Fragment object
+  //Creating a dl element to hold the athlete's information, and extract the day, month, and year from the date variable
+  const list = document.createElement('dl');        
+  const day =  date.getDate()                       
+  const month = MONTHS[date.getMonth()];
+  const year = date.getFullYear();
+  console.log(month)
+   // calculating the total time of the athlete's latest race useing a loop to add up all the times
+  let sum = 0   
+  let i = 0;
+  while ( i < time.length ) {
+    sum += time[i]
+    i++
+  }
+  //createing a string of HTML code using a template literal, and assigns it to the innerHTML property of the list element. 
+  const minutes = sum % 60;
+  const hours = (sum - minutes) / 60;
+  list.innerHTML = /* html */ `
+      <dt>Athlete: ${firstName +' '+ surname}</dt>
+      <dt>Total Races: ${races}</dt>
+      <dt>Event Date (Latest): ${day.toString().padStart(2, '0')+' '+ month +' '+ year}</dt>
+      <dt>Total Time (Latest): ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}</dt>
+    `;
+  fragment.appendChild(list);   //appending  the list element to the DocumentFragment 
+  return fragment;              //return the DocumentFragment.
 }
+// select two elements using their data-athlete attribute, and append the HTML created by the createHtml function to those elements
+document.querySelector('[data-athlete = "NM372"]').appendChild(createHtml('NM372'));
+document.querySelector('[data-athlete ="SV782"]').appendChild(createHtml('SV782'));
 
-Onthatile();
 
 
-
-  // const createHtml = (athlete) => {
-  //   firstName, surname, id, races = athlete
-  //   [date], [time] = races.reverse()
   
-  //   const fragment = document.createDocumentFragment();
-  
-  //   title = document.createElement(h2);
-  //   title= id;
-  //   fragment.appendChild(title);
-  
-  //   const list = document.createElement(dl);
-  
-  //   const day = date.getDate();
-  //   const month = MONTHS[date.month];
-  //   const year = date.year;
-  
-  //   first, second, third, fourth = timeAsArray;
-  //   total = first + second + third + fourth;
-  
-  //   const hours = total / 60;
-  //   const minutes = total / hours / 60;
-  
-  
-  //   list.innerHTML = /* html */ `
-  //     <dt>Athlete</dt>
-  //     <dd>${firstName surname}</dd>
-  
-  //     <dt>Total Races</dt>
-  //     <dd>${races}</dd>
-  
-  //     <dt>Event Date (Latest)</dt>
-  //     <dd>${day month year}</dd>
-  
-  //     <dt>Total Time (Latest)</dt>
-  //     <dd>${hours.padStart(2, 0) minutes}</dd>
-  //   `;
-  
-  //   fragment.appendChild(list);
-  // }
